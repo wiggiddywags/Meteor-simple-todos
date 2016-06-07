d13e151e6331794439ce1ed5edd6f7e671998a4c
Jwaggoner <wiggiddywags@gmail.com>
2016-06-07 16:41:34 -0400

    begin add task form submit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,22 @@ Template.body.helpers({
     return Tasks.find({});
   }
 });
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    //Insert a task into the Collection
+    Tasks.insert({
+        text, createdAt: new Date(), //current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+});
