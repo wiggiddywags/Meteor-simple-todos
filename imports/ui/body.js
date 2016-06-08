ea3ed76d28e372030ee691066eed629d349acab2
Jwaggoner <wiggiddywags@gmail.com>
2016-06-08 12:19:37 -0400

    Added task complete toggle; fixed hide-complete bug

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,27 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
 
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if(instance.state.get('hideCompleted')) {
+      //if hide completed is checked, filter tasks
+      return Tasks.find({checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    //Otherwise, return all of the tasks; show newest tasks at the top
     return Tasks.find({}, {sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -26,5 +41,8 @@ Template.body.events({
 
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
